933e34d3a7ac7697b67dc9012d6f20b1eba97c6b
dankuri <danek.il420@gmail.com>
2024-01-18 22:43:53 +0100

    add yanked text highlighting

--- config/default.nix
+++ config/default.nix
@@ -71,5 +71,15 @@
       opts.border = opts.border or "rounded"
       return orig_util_open_floating_preview(contents, syntax, opts, ...)
     end
+
+    -- highlight yanked text
+    vim.api.nvim_create_autocmd("TextYankPost", {
+      group = vim.api.nvim_create_augroup("highlight_yank", {}),
+      desc = "Hightlight selection on yank",
+      pattern = "*",
+      callback = function()
+          vim.highlight.on_yank({ higroup = "IncSearch", timeout = 100 })
+      end,
+    })
   '';
 }
